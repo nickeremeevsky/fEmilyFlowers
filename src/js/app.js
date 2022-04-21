cea1778e625ee47ca5d2fdc56a2e7c1d7281e7b8
Nick Eremeevsky <nick.eremeevsky@yandex.ru>
2022-04-21 15:01:00 +0300

    fix style.scss errors, add new images, add scrollreveal in project

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+import ScrollReveal from 'scrollreveal';
+
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
   navClose = document.getElementById('nav-close');
@@ -130,4 +132,22 @@ themeButton.addEventListener('click', () => {
   // We save the theme and the current icon that the user chose
   localStorage.setItem('selected-theme', getCurrentTheme());
   localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// scroll reval animation
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+  // reset: true
+});
+
+sr.reveal(`.home__data`);
+sr.reveal(`.home__img`, { delay: 500 });
+sr.reveal(`.home__social`, { delay: 600 });
+sr.reveal(`.about__img, .contact__box`, { origin: 'left' });
+sr.reveal(`.about__data, .contact__form`, { origin: 'right' });
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {
+  interval: 100,
 });
